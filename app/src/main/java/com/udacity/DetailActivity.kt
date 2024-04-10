f24309a0d2e86a6c26187085667b4be6023aa9e5
Adriano Tumminelli <a.tumminelli@reply.it>
2024-04-10 22:29:40 +0200

    feat: download manager and detail screen with animation

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,5 +1,6 @@
 package com.udacity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.udacity.databinding.ActivityDetailBinding
@@ -13,5 +14,32 @@ class DetailActivity : AppCompatActivity() {
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        val filename = intent.getStringExtra("filename")
+        if (!filename.isNullOrEmpty()) {
+            binding.contentDetail.textFilenameValue.text = filename
+        }
+
+        val status = intent.getStringExtra("status")
+        if (!status.isNullOrEmpty()) {
+            val textStatus = binding.contentDetail.textStatusValue
+            textStatus.text = status
+            textStatus.setTextColor(when (status) {
+                getString(R.string.success) -> resources.getColor(R.color.green, null)
+                getString(R.string.fail) -> resources.getColor(R.color.red, null)
+                else -> resources.getColor(R.color.red, null)
+            })
+        }
+
+        binding.contentDetail.buttonOk.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    MainActivity::class.java
+                )
+            )
+            //close current activity
+            finish()
+        }
     }
 }
--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -1,25 +1,36 @@
 package com.udacity
 
+import android.Manifest
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import com.udacity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private var downloadID: Long = 0
+    private var downloadID: Long = -1
+    private var selectedFile = ""
 
+    val NOTIFICATION_ID = 0
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
@@ -30,6 +41,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
+
+        initNotifications()
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         binding.contentMain.loadingButton.setOnClickListener {
@@ -42,7 +55,7 @@ class MainActivity : AppCompatActivity() {
                 ).show()
             } else if(downloadID > 0) {
                 button.cancelProgressAnimation()
-                downloadID = 0;
+                downloadID = -1;
             } else {
                 val url = when (binding.contentMain.choices.checkedRadioButtonId) {
                     R.id.rb_app -> URL_APP
@@ -50,12 +63,35 @@ class MainActivity : AppCompatActivity() {
                     R.id.rb_retrofit -> URL_RETROFIT
                     else -> ""
                 }
+                val radioButton = findViewById<RadioButton>(binding.contentMain.choices.checkedRadioButtonId)
+                selectedFile = radioButton.text.toString()
                 download(url)
                 button.startProgressAnimation()
             }
         }
     }
 
+    private fun initNotifications() {
+        notificationManager = ContextCompat.getSystemService(
+            this,
+            NotificationManager::class.java
+        ) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                applicationContext.getText(R.string.notification_channel_name),
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = applicationContext.getString(R.string.notification_description)
+                lightColor = Color.GREEN
+                enableVibration(true)
+                enableLights(true)
+            }
+
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
@@ -73,17 +109,54 @@ class MainActivity : AppCompatActivity() {
                             DownloadManager.STATUS_SUCCESSFUL -> {
                                 downloadID = 0;
                                 binding.contentMain.loadingButton.endProgressAnimation()
-                                Toast.makeText(applicationContext, "Download success", Toast.LENGTH_SHORT).show()
+
+
+                                sendNotification(selectedFile, getString(R.string.success))
+
                             }
                             DownloadManager.STATUS_FAILED -> {
                                 downloadID = 0;
                                 binding.contentMain.loadingButton.endProgressAnimation()
                                 Toast.makeText(applicationContext, "Download failed", Toast.LENGTH_SHORT).show()
+
+                                sendNotification(selectedFile, getString(R.string.fail))
                             }
                         }
                     }
                     cursor.close()
                 }
+            }
+        }
+    }
+
+    private fun sendNotification(fileName: String, status: String) {
+
+        val intent = Intent(applicationContext, DetailActivity::class.java)
+        intent.putExtra("filename", fileName)
+        intent.putExtra("status", status)
+
+
+        val contentIntent = PendingIntent.getActivity(
+            applicationContext,
+            NOTIFICATION_ID,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+        )
+
+        val builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_assistant_black_24dp)
+            .setContentTitle(applicationContext.getText(R.string.notification_title))
+            .setContentText(applicationContext.getText(R.string.notification_description))
+            .setContentIntent(contentIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .addAction(R.drawable.ic_assistant_black_24dp, applicationContext.getString(R.string.notification_button), contentIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(applicationContext)) {
+            if (ActivityCompat.checkSelfPermission(this@MainActivity, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                notify(NOTIFICATION_ID, builder.build())
+            } else {
+                Toast.makeText(applicationContext, "Please enable notifications", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -105,7 +178,7 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val URL_APP = "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val URL_GLIDE = "https://github.com/bumptech/glide/archive/master.zip"
-        private const val URL_RETROFIT = "https://github.com/square/retrofit/archive/master.zip"
+        private const val URL_RETROFIT = "https://github.com/square/retrofit/archive/master.zi" //error on purpose to make it fail
         private const val CHANNEL_ID = "channelId"
     }
 }
